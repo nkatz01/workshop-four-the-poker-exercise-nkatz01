ce6cc5a22923632fd37c9149d6b400a1fd303114
Keith Mannock <keith@dcs.bbk.ac.uk>
2020-02-08 14:09:25 +0000

    sync

---
+++ Workshop/Poker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
