4e2c0fcd20cbbf2cf5ebea087609c3dd5336a811
unknown <Nuchem@CS00041648.ad.ucl.ac.uk>
2020-02-25 13:25:01 +0000

    recommitted and dumped away gitignore as it prevented from valid test sourcecode from being pushed

---
+++ Workshop/Poker.Tests/CardTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using static Poker.Card;
+
+namespace Poker.Tests
+{
+    [TestClass]
+    public class CardTests
+    {
+        [TestMethod]
+        public void CanCreateCardWithValue()
+        {
+           var card = new Card(CardValue.Ace, CardSuit.Clubs);
+
+          //  var card = new Card( );
+            Assert.IsNotNull(card);
+            Assert.AreEqual(CardSuit.Clubs, card.Suit);
+            Assert.AreEqual(CardValue.Ace, card.Value);
+        }
+
+
+        [TestMethod]
+        public void CanDescribeCard()
+        {
+            var card = new Card(CardValue.Ace, CardSuit.Spades);
+            Assert.AreEqual("Ace of Spades", card.ToString());
+        }
+
+      
+
+
+
+    }
+}
---
+++ Workshop/Poker.Tests/HandTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using static Poker.Card;
+using static Poker.Hand;
+namespace Poker.Tests
+{
+    [TestClass]
+   public class HandTests
+    {
+
+        [TestMethod]
+        public void CanCreateHand()
+        {
+            var hand = new Hand();
+            Assert.IsTrue(hand.Cards.Count == 0);
+
+        }
+        [TestMethod]
+        public void CanHandDrawCard()
+        {
+
+            var card = new Card(Card.CardValue.Ace, Card.CardSuit.Spades);
+            var hand = new Hand();
+            hand.Draw(card);
+            Assert.AreEqual(hand.Cards.First(), card);
+        }
+
+        [TestMethod]
+        public void CanGetHighCard()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Seven, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Five, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.King, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Two, CardSuit.Hearts));
+            Assert.AreEqual(CardValue.King, hand.HighCard().Value);
+        }
+
+        [TestMethod]
+        public void CanScoreHighCard()
+        {
+            var hand = new Hand();
+            //hand.Draw(new Card(CardValue.Seven, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            //    hand.Draw(new Card(CardValue.Five,  CardSuit.Hearts));
+            //    hand.Draw(new Card(CardValue.King,  CardSuit.Hearts));
+            //    hand.Draw(new Card(CardValue.Two,   CardSuit.Hearts));
+            Assert.AreEqual(HandRank.HighCard, hand.GetHandRank());
+        }
+
+        [TestMethod]
+        public void CanScoreFlush()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Two, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Three, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Five, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Six, CardSuit.Spades));
+            Assert.AreEqual(HandRank.Flush, hand.GetHandRank());
+        }
+
+        [TestMethod]
+        public void CanScoreRoyalFlush()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Queen, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.King, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+            Assert.AreEqual(HandRank.RoyalFlush, hand.GetHandRank());
+        }
+
+
+
+    }
+}
